7c0a7f18cbb69e1ddbad5f3ab1b60ef074c11789
Juan Pablo Guzmán Fernández <juanpyguzman@gmail.com>
2020-02-29 17:31:10 +0100

    Movimiento del Box Collider de la araña al atacar añadido

--- Assets/Models/Spider/SpiderControllerScript.cs
+++ Assets/Models/Spider/SpiderControllerScript.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class SpiderControllerScript : MonoBehaviour
 {
-    /*static int speedHash = Animator.StringToHash("Speed");
+
+    static int speedHash = Animator.StringToHash("Speed");
+    static int attackHash = Animator.StringToHash("Attack");
     public Animator animComponent = null;
+
+    BoxCollider box;
+    float original_z = 0;
 
     float verticalAxis = 0;
     float horizontalAxis = 0;
 
-    // Update is called once per frame
+
+    void Start()
+    {
+        box = GetComponent<BoxCollider>();
+        original_z = box.center.z;
+        animComponent = GetComponent<Animator>();
+    }
+
     void Update()
     {
+        //Movimiento
         verticalAxis = Input.GetAxis("Vertical");
         horizontalAxis = Input.GetAxis("Horizontal");
 
+        //Ataque
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animComponent.SetTrigger(attackHash);
+        }
+
         animComponent.SetFloat(speedHash, verticalAxis);
-    }*/
+
+        //Movimiento del collider al atacar
+        box.center = new Vector3(box.center.x, box.center.y, original_z + animComponent.GetFloat("Distance")*10.0f);
+    }
 }
