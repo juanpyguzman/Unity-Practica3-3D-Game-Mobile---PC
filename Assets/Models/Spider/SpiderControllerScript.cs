9e090494e223c910bb394309f110654be8a792d3
Juan Pablo Guzmán Fernández <juanpyguzman@gmail.com>
2020-02-29 16:38:54 +0100

    Araña animada

---
+++ Assets/Models/Spider/SpiderControllerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderControllerScript : MonoBehaviour
+{
+    /*static int speedHash = Animator.StringToHash("Speed");
+    public Animator animComponent = null;
+
+    float verticalAxis = 0;
+    float horizontalAxis = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalAxis = Input.GetAxis("Vertical");
+        horizontalAxis = Input.GetAxis("Horizontal");
+
+        animComponent.SetFloat(speedHash, verticalAxis);
+    }*/
+}
